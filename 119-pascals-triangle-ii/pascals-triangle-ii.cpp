1267429f53c09b242b26015e905816aaa6b74a92
Arpit Jana <106902674+arpitjana2103@users.noreply.github.com>
2024-08-24 08:19:12 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (78.52%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -4,12 +4,12 @@ public:
         int n = rowIndex + 1;
         vector<int> res(n, 0);
         res[0] = 1;
-        
+
         for(int i = 2; i<=n; i++){
             int prev = res[0];
             for(int j = 1; j<i; j++){
                 int temp = res[j];
-                res[j] += prev;
+                res[j] = res[j] + prev;
                 prev = temp;
             }
         }
