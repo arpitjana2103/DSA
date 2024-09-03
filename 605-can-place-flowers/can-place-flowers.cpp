6c32fc1d685f1fdcdcd8aea8c8f02270626a7d04
Arpit Jana <106902674+arpitjana2103@users.noreply.github.com>
2024-09-03 10:22:02 +0530

    Time: 27 ms (9.16%) | Memory: 23.1 MB (52.91%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.cpp
+++ 605-can-place-flowers/can-place-flowers.cpp
@@ -4,15 +4,15 @@ bool canPlaceFlowers(const vector<int>& flowerbed, int n) {
         int count = 0;
         int size = flowerbed.size();
         
-        for (int i = 0; i < size; ++i) {
+        for (int i = 0; i < size; i++) {
             if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == size - 1 || flowerbed[i + 1] == 0)) {
+                cout << i << endl;
                 count++;
-                i++; // Skip the next spot as it's adjacent to the planted one
-                if (count >= n) {
-                    return true;
-                }
+                i++;
             }
         }
+
+        cout << count;
         
         return count >= n;
     }
