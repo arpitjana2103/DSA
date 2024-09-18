6084c8f635c37963e3f9794135c013e21a5ff0b9
Arpit Jana <106902674+arpitjana2103@users.noreply.github.com>
2024-09-18 12:12:38 +0530

    Time: 8 ms (98.05%) | Memory: 12.3 MB (76.39%) - LeetSync

--- 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -1,37 +1,29 @@
 class Solution {
 public:
     string removeDuplicates(string s, int k) {
-        stack<pair<char, int>> st;
+        vector<pair<char, int>> st;
 
         for(char ch:s){
-            if(!st.empty() && st.top().first == ch){
-                int count = st.top().second + 1;
-                st.pop();
-                if(count < k) st.push(make_pair(ch, count));
+            if(!st.empty() && st.back().first == ch){
+                int count = st.back().second + 1;
+                st.pop_back();
+                if(count < k) st.push_back(make_pair(ch, count));
             }else{
-               st.push(make_pair(ch, 1)); 
+               st.push_back(make_pair(ch, 1)); 
             }
         }
 
+
+        // Building res
         string res;
 
-        while(!st.empty()){
-            char ch = st.top().first;
-            int count = st.top().second;
-            st.pop();
+        for(auto& pair:st){
+            char ch = pair.first;
+            int count = pair.second;
 
             for(int i = 0; i<count; i++){
                 res += ch;
             }
-        }
-
-        int left = 0;
-        int right = res.size()-1;
-
-        while(left <= right){
-            swap(res[left], res[right]);
-            left++;
-            right--;
         }
 
         return res;
