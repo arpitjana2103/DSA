c4a23c5421ca933a2a8a1196fdf4634edc825323
Arpit Jana <106902674+arpitjana2103@users.noreply.github.com>
2025-04-10 14:48:16 +0530

    0057-INSERT-INTERVAL Stats: Time: 5 ms (12.41%), Space: 22.4 MB (7.2%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,24 +1,37 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        vector<vector<int>> res = {};
+        
+        vector<vector<int>> res;
 
-        for(int i = 0; i < intervals.size(); i++){
-            vector<int> currInterval = intervals[i];
+        int i = 0;
 
-            if(newInterval.at(1) < currInterval.at(0)){
+        while(i < intervals.size()){
+            vector<int> cInterval = intervals[i];
+
+            // Case 01 :: [newInterval], [cInterval]
+            if(newInterval[1] < cInterval[0]){
                 res.push_back(newInterval);
                 res.insert(res.end(), intervals.begin() + i, intervals.end());
                 return res;
-            }else if(newInterval.at(0) > currInterval.at(1)){
-                res.push_back(currInterval);
-            }else {
-                newInterval[0] = min(newInterval[0], currInterval[0]);
-                newInterval[1] = max(newInterval[1], currInterval[1]);
             }
+
+            // Case 02 :: [cInterval], [newInterval]
+            else if(cInterval[1] < newInterval[0]){
+                res.push_back(cInterval);
+            }
+
+            // Case 03 :: cInterval & newInterval are colliding
+            else {
+                newInterval[0] = min(cInterval[0], newInterval[0]);
+                newInterval[1] = max(cInterval[1], newInterval[1]);
+            }
+
+            i++;
         }
 
         res.push_back(newInterval);
+
         return res;
     }
 };
