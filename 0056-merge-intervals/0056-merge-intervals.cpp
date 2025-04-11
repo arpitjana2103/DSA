10b1fcaffddf002f337ed8f78bd84466311d5c32
Arpit Jana <106902674+arpitjana2103@users.noreply.github.com>
2025-04-11 17:43:35 +0530

    0056-MERGE-INTERVALS Stats: Time: 3 ms (89.63%), Space: 24 MB (35.34%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -3,13 +3,15 @@ public:
     static bool compFn(vector<int>& a, vector<int>& b){
         return a[0] < b[0];
     }
+
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         // Sort the intervals vector
         sort(intervals.begin(), intervals.end(), compFn);
+
         vector<vector<int>> res;
         res.push_back(intervals[0]);
 
-        for(int i = 0; i < intervals.size(); i++){
+        for(int i = 0; i<intervals.size(); i++){
             int& lastEnd = res.at(res.size()-1).at(1);
             int start = intervals.at(i).at(0);
             int end = intervals.at(i).at(1);
@@ -18,7 +20,7 @@ public:
                 res.push_back(intervals.at(i));
             }else{
                 lastEnd = max(lastEnd, end);
-            }   
+            }
         }
 
         return res;
