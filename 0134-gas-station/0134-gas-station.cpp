d7e22bbcf1d34d1036c22faa3cebdc6ffb4f5d53
Arpit Jana <106902674+arpitjana2103@users.noreply.github.com>
2025-04-11 09:45:55 +0530

    0134-GAS-STATION Stats: Time: 7 ms (12.45%), Space: 112.5 MB (42.45%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int n = cost.size();
+
         int fuelLeft = 0;
         int currFuel = 0;
         int res = 0;
-
-        for(int i = 0; i<n; i++){
+        
+        for(int i = 0; i<gas.size(); i++){
             int diff = gas[i] - cost[i];
             fuelLeft += diff;
             currFuel += diff;
 
             if(currFuel < 0){
                 currFuel = 0;
-                res = i+1;
+                res = i + 1;
             }
+
         }
 
         if(fuelLeft < 0) return -1;
-        return res;
+        else return res;
     }
 };
