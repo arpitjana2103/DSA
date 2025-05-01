9ea71ece22943960b8aecb8814ecd0e0b74925f6
Arpit Jana <106902674+arpitjana2103@users.noreply.github.com>
2025-05-01 14:20:04 +0530

    0075-SORT-COLORS Stats: Time: 0 ms (100%), Space: 11.7 MB (12.09%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
+    void printVector(vector<int>& nums){
+        for(int num:nums){
+            cout << num << " ";
+        }
+        cout << endl;
+    }
     void sortColors(vector<int>& nums) {
-        int n = nums.size();
-        int lo = 0;
+        int left = 0;
+        int right = nums.size() - 1;
         int mid = 0;
-        int hi = n - 1;
 
-        while(mid <= hi){
-            if(nums[mid] == 2){
-                swap(nums[mid], nums[hi]);
-                hi--;
-            }else if(nums[mid] == 1){
+        while(mid <= right){
+            if(nums[mid] == 2) 
+                swap(nums[right--], nums[mid]);
+            else if(nums[mid] == 0) 
+                swap(nums[left++], nums[mid++]); 
+            else if(nums[mid] == 1) 
                 mid++;
-            }else if(nums[mid] == 0){
-                swap(nums[mid], nums[lo]);
-                lo++;
-                mid++;
-            }
         }
     }
 };
