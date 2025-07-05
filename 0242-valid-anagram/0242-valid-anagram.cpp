6f963020529121de49b49590b911497ea25c6a43
Arpit Jana <106902674+arpitjana2103@users.noreply.github.com>
2025-07-05 18:55:09 +0530

    0242-VALID-ANAGRAM Stats: Time: 1 ms (61.37%), Space: 10 MB (11.88%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,19 +2,21 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.size() != t.size()) return false;
+        unordered_map<char, int> smp, tmp;
 
-        unordered_map<char, int> sMap;
-        unordered_map<char, int> tMap;
-
-        for(int i = 0; i<s.size(); i++){
-            sMap[s[i]]++;
-            tMap[t[i]]++;
+        for(char ch:s){
+            smp[ch]++;
         }
 
-        for(auto& pair:sMap){
-            char ch = pair.first;
-            int count = pair.second;
-            if(tMap[ch] != count) return false;
+        for(char ch:t){
+            tmp[ch]++;
+        }
+
+        for(auto &pair:smp){
+            int sCount = pair.second;
+            int tCount = tmp[pair.first];
+
+            if(sCount != tCount) return false;
         }
 
         return true;
