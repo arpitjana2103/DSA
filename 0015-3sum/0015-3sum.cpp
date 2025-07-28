83084747567935c2d78a9f8db8854d382449d638
Arpit Jana <106902674+arpitjana2103@users.noreply.github.com>
2025-07-28 20:07:08 +0530

    0015-3SUM Stats: Time: 59 ms (27.16%), Space: 29.2 MB (40.24%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -7,8 +7,8 @@ public:
 
         for(int i = 0; i<n; i++){
             if(i > 0 && nums[i] == nums[i-1]) continue;
-            int target = -nums[i];
 
+            int target = -nums[i];
             int lo = i+1;
             int hi = n-1;
 
@@ -16,7 +16,7 @@ public:
                 int sum = nums[lo] + nums[hi];
                 if(sum < target) lo++;
                 if(sum > target) hi--;
-                if(sum == target){
+                if(sum == target) {
                     res.push_back({-target, nums[lo], nums[hi]});
                     while(lo < hi && nums[lo] == nums[lo+1]) lo++;
                     while(lo < hi && nums[hi] == nums[hi-1]) hi--;
