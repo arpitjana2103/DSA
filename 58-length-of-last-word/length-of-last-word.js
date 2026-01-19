79af68907d6d0421891ca302d84716469ace4f3a
Arpit Jana <106902674+arpitjana2103@users.noreply.github.com>
2026-01-19 10:40:42 +0530

    Time: 0 ms (100.00%) | Memory: 53.7 MB (63.87%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -2,7 +2,24 @@
  * @param {string} s
  * @return {number}
  */
+
+// Sol 1 : TC On, SC On
+// var lengthOfLastWord = function(s) {
+//     return s.split(" ").filter(str => str !== "").at(-1).length;
+// };
+
 var lengthOfLastWord = function(s) {
-    console.log(s.split(" "));
-    return s.split(" ").filter(str => str !== "").at(-1).length;
+    let prevChar = " ";
+    let i = s.length - 1;
+    let count = 0;
+
+    while(i >= 0){
+        const ch = s.at(i);
+        if(ch === " " && prevChar !== " ") return count;
+        if(ch !== " ") count++;
+        prevChar = ch;
+        i--;
+    }
+
+    return count;
 };
